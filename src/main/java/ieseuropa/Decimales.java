61c86cbd74f4777b0d60f09b0b8f96023d3a1b9a
dev <dev@debian>
2023-03-24 19:15:01 +0100

    Ecuacion Segundo Grado

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -1,1 +1,19 @@
+package main.java.ieseuropa;
 
+public class Decimales {
+
+	public static double segundoGrado(double a, double b, double c) {
+		return (-b + Math.sqrt((b*b) - (4*a*c)) )/ 2*a;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("-----------------> ejecutar la ecuación de segundo grado ");
+		System.out.println(segundoGrado(1, -2, 1));
+		System.out.println(segundoGrado(1, -4, 4));
+		System.out.println(segundoGrado(-1, 4, -4));
+	}
+
+}
