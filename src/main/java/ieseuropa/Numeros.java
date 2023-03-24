97f2e1678e01393872e407bcc352fd25cfd5b86e
AitorSV <aitorsuarez.vic@gmail.com>
2023-03-24 18:17:34 +0100

    actualizado con funcion de horas/min/sec

--- src/main/java/ieseuropa/Numeros.java
+++ src/main/java/ieseuropa/Numeros.java
@@ -71,6 +71,16 @@ public class Numeros {
 		return lista;
 	}
 	
+	private static String calcularHora(int num) {
+		int horas = num / 3600;
+		int segundos = num % 3600;
+		int minutos = segundos / 60;
+		segundos = segundos % 60;
+		String hora = String.valueOf(horas + "hs ").concat(String.valueOf(minutos + "mins "))
+				.concat(segundos + "segs ");
+		return hora;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(calcularFactorial(12));
@@ -85,6 +95,7 @@ public class Numeros {
 			System.out.println(prueba.get(i));
 		}
 		
+		System.out.println(calcularHora(9000));
 	}
 
 }
