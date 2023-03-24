d16b8be1feb5f377b089798781a8603ea8439e6b
dev <dev@debian>
2023-03-24 18:42:03 +0100

    Decimales Alza Generados

--- src/main/java/ieseuropa/Decimales_Al_Alza.java
+++ src/main/java/ieseuropa/Decimales_Al_Alza.java
@@ -8,7 +8,7 @@ public class Decimales_Al_Alza {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		System.out.println("-----------------> Decimales Redondear el número al alza");
 		System.out.println(redondearArriba(3.1415));
 		System.out.println(redondearArriba(1.4563));
